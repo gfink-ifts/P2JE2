818ebcfdd0329c3872d3e597e0c2f51a8598e65c
Daniel Banda <cdbanda@gmail.com>
2020-11-04 00:00:54 -0300

    Formulario Ventas - Llenar Grilla

--- Equipo2/Equipo2/Ventas.Designer.cs
+++ Equipo2/Equipo2/Ventas.Designer.cs
@@ -28,12 +28,74 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.groupBox_datosVenta = new System.Windows.Forms.GroupBox();
+            this.dataGridView_ventas = new System.Windows.Forms.DataGridView();
+            this.button_nuevaVenta = new System.Windows.Forms.Button();
+            this.button_cerrar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_ventas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox_datosVenta
+            // 
+            this.groupBox_datosVenta.Location = new System.Drawing.Point(13, 13);
+            this.groupBox_datosVenta.Name = "groupBox_datosVenta";
+            this.groupBox_datosVenta.Size = new System.Drawing.Size(641, 173);
+            this.groupBox_datosVenta.TabIndex = 0;
+            this.groupBox_datosVenta.TabStop = false;
+            this.groupBox_datosVenta.Text = "Datos de la Venta";
+            // 
+            // dataGridView_ventas
+            // 
+            this.dataGridView_ventas.AllowUserToAddRows = false;
+            this.dataGridView_ventas.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView_ventas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView_ventas.Location = new System.Drawing.Point(13, 192);
+            this.dataGridView_ventas.Name = "dataGridView_ventas";
+            this.dataGridView_ventas.RowHeadersVisible = false;
+            this.dataGridView_ventas.Size = new System.Drawing.Size(775, 246);
+            this.dataGridView_ventas.TabIndex = 1;
+            // 
+            // button_nuevaVenta
+            // 
+            this.button_nuevaVenta.Location = new System.Drawing.Point(692, 56);
+            this.button_nuevaVenta.Name = "button_nuevaVenta";
+            this.button_nuevaVenta.Size = new System.Drawing.Size(75, 50);
+            this.button_nuevaVenta.TabIndex = 2;
+            this.button_nuevaVenta.Text = "Nueva Venta";
+            this.button_nuevaVenta.UseVisualStyleBackColor = true;
+            this.button_nuevaVenta.Click += new System.EventHandler(this.button_nuevaVenta_Click);
+            // 
+            // button_cerrar
+            // 
+            this.button_cerrar.Location = new System.Drawing.Point(692, 451);
+            this.button_cerrar.Name = "button_cerrar";
+            this.button_cerrar.Size = new System.Drawing.Size(84, 23);
+            this.button_cerrar.TabIndex = 3;
+            this.button_cerrar.Text = "Cerrar";
+            this.button_cerrar.UseVisualStyleBackColor = true;
+            this.button_cerrar.Click += new System.EventHandler(this.button_cerrar_Click);
+            // 
+            // Ventas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 486);
+            this.Controls.Add(this.button_cerrar);
+            this.Controls.Add(this.button_nuevaVenta);
+            this.Controls.Add(this.dataGridView_ventas);
+            this.Controls.Add(this.groupBox_datosVenta);
+            this.Name = "Ventas";
             this.Text = "Ventas";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_ventas)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox_datosVenta;
+        private System.Windows.Forms.DataGridView dataGridView_ventas;
+        private System.Windows.Forms.Button button_nuevaVenta;
+        private System.Windows.Forms.Button button_cerrar;
     }
 }
--- Equipo2/Equipo2/Ventas.cs
+++ Equipo2/Equipo2/Ventas.cs
@@ -7,14 +7,61 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace Equipo2
 {
     public partial class Ventas : Form
     {
+        //INICIO CONEXION
+        SqlConnection conexion;
+
+        public void Abrir()
+        {
+            conexion = new SqlConnection();
+            conexion.ConnectionString = @"Integrated Security=SSPI;Initial Catalog=TiendaInformatica;Data Source=HPMINI";
+            conexion.Open();
+        }
+
+        public void Cerrar()
+        {
+            conexion.Close();
+            conexion.Dispose();
+            conexion = null;
+            GC.Collect();
+        }
+
+        //FIN CONEXION
         public Ventas()
         {
             InitializeComponent();
+            LlenarGrilla();
+        }
+
+        private void button_nuevaVenta_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LlenarGrilla() {
+
+            SqlDataAdapter da;
+            DataTable dt = new DataTable();
+            string consulta = "select v.fechaVenta,c.nombre, c.apellido, p.descripcion , p.precio, v.cantidad, (v.cantidad * p.precio) as Total " +
+                                "from ventas as v, Clientes as c, Productos as p " +
+                                "where v.idcliente = c.idCliente and v.idproducto = p.idProducto";
+            Abrir();
+            da = new SqlDataAdapter(consulta, conexion);
+            da.Fill(dt);
+            Cerrar();
+            dataGridView_ventas.DataSource = dt;
+
+        }
+
+        private void button_cerrar_Click(object sender, EventArgs e)
+        {
+            Close();
+            GC.Collect();
         }
     }
 }
